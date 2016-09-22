534c928b09c5fbe6cd09d644353916b6ce88ea74
Gus Caplan <fluffyrobotcheese@gmail.com>
2016-09-22 01:50:35 -0500

    i dunno

--- test.js
+++ test.js
@@ -20,6 +20,6 @@ console.log('hello Gus, you are in ultimate-shitposting random number 3 AAAhi!!!
 console.log(out.data);
 console.log('tag with name \'test\' exists:', tags.exists('test'));
 console.log('tag with name \'o3u2fweksdbj\' exists:', tags.exists('o3u2fweksdbj'))
-console.log(tags.keys());
+console.log(tags.keys().join(', ').substring(0, 2000));
 tags.remove('test');
 console.log('removed tag test, so exists is now', tags.exists('test'));
