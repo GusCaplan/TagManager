1f2518ad83bec6d446bb73eeb053926c224fa084
Gus Caplan <fluffyrobotcheese@gmail.com>
2016-09-22 00:20:30 -0500

    fix exists

--- TagManager.js
+++ TagManager.js
@@ -29,7 +29,7 @@ class TagManager {
   }
 
   exists (key) {
-    return (this.storage.getItem(key) === null);
+    return (this.storage.getItem(key) !== null);
   }
 }
 
--- test.js
+++ test.js
@@ -16,5 +16,7 @@ const functions = {
 
 let out = tags.get('test', replace, functions);
 console.log('The next two lines should equal each other except for the random number ;)')
-console.log('hello Gus, you are in ultimate-shitposting random number 3 AAAhi!!!')
+console.log('hello Gus, you are in ultimate-shitposting random number 3 AAAhi!!!');
 console.log(out.data);
+console.log('tag with name \'test\' exists:', tags.exists('test'));
+console.log('tag with name \'o3u2fweksdbj\' exists:', tags.exists('o3u2fweksdbj'))
