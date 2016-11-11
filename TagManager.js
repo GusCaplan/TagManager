700d61fc6948ea5349059c9eb48f0a47135fb7d1
Gus Caplan <fluffyrobotcheese@gmail.com>
2016-11-11 17:41:59 -0600

    memesndreams

--- TagManager.js
+++ TagManager.js
@@ -1,8 +1,5 @@
 const LocalStorage = require('node-localstorage').LocalStorage;
-const TagScript = require('tagscript');
-const compiler = new TagScript();
-const ohwait = require('asyncawait/await');
-const ohsync = require('asyncawait/async');
+const Handlebars = require('handlebars');
 
 class TagManager {
   constructor (options = {}) {
@@ -11,24 +8,24 @@ class TagManager {
     this.functions = options.functions || {};
   }
 
-  set (key, value, meta) {
-    let data = { data: value, meta: meta };
+  set (key, content, meta) {
+    let data = { content, meta };
     return this.storage.setItem(key, JSON.stringify(data));
   }
 
   get (key, functions = {}) {
-    return ohsync(() => {
-      functions = Object.assign(functions, this.functions);
-      let data = JSON.parse(this.storage.getItem(key));
-      if (!data) return undefined;
-      data.data = ohwait(compiler.compile(data.data, functions));
-      return data;
-    })();
+    functions = Object.assign(functions, this.functions);
+    for (const fn in functions) Handlebars.registerHelper(fn, functions[fn]);
+    let data = JSON.parse(this.storage.getItem(key));
+    if (!data) return undefined;
+    data.content = Handlebars.compile(data.content)();
+    // data.data = ohwait(compiler.compile(data.data, functions));
+    return data;
   }
 
   raw (key) {
     let data = JSON.parse(this.storage.getItem(key));
-    return data.data;
+    return data;
   }
 
   remove (key) {
--- test.js
+++ test.js
@@ -1,11 +1,9 @@
 const TagManager = require('./TagManager');
-const async = require('asyncawait/async');
-const await = require('asyncawait/await');
 
 const functions = {
   'name': () => 'Gus',
   'channel': () => 'ultimate-shitposting',
-  'omfg': (arg1) => 'AAA' + arg1 + '!!!',
+  'omfg': (...args) => `OMG! ${args[0].toUpperCase()}!!!`,
   'random': (min, max) => Math.floor(Math.random() * parseInt(max)) + parseInt(min)
 }
 
@@ -13,13 +11,12 @@ const tags = new TagManager({
   functions: functions
 });
 
-async(() => {
-  tags.set('test', 'hello {name}, you are in {channel} random number {random;1;4} {omfg;hi}', {author: '1234'});
+(() => {
+  tags.set('test', 'hello {{name}}, you are in {{channel}} random number {{random 1 4}} {{omfg "hi"}}', {author: '1234'});
   tags.set('o shit', 'hello');
-  let out = await(tags.get('test'));
   console.log('The next two lines should equal each other except for the random number ;)')
-  console.log('hello Gus, you are in ultimate-shitposting random number 3 AAAhi!!!');
-  console.log(out.data);
+  console.log('hello Gus, you are in ultimate-shitposting random number 3 OMG! HI!!!');
+  console.log(tags.get('test').content);
   console.log('tag with name \'test\' exists:', tags.exists('test'));
   console.log('tag with name \'o3u2fweksdbj\' exists:', tags.exists('o3u2fweksdbj'))
   console.log('TAGS:', tags.keys().join(', ').substring(0, 2000));
